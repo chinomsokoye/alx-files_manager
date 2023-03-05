b7f0df0a1d01a0cb0d95b010b535473f0007d6fa
nomso <chinomsokoye@yahoo.com>
2023-03-05 13:47:14 -0800

    Create First API

---
+++ routes/index.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
+
+const router = express.Router();
+
+const routeController = (app) => {
+  app.use('/', router);
+
+  router.get('/status', (req, res) => {
+    AppController.getStatus(req, res);
+  });
+
+  router.get('/stats', (req, res) => {
+    AppController.getStats(req, res);
+  });
+
+  router.post('/users', (req, res) => {
+    UsersController.postNew(req, res);
+  });
+
+  router.get('/connect', (req, res) => {
+    AuthController.getConnect(req, res);
+  });
+
+  router.get('/disconnect', (req, res) => {
+    AuthController.getDisconnect(req, res);
+  });
+
+  router.get('/users/me', (req, res) => {
+    UsersController.getMe(req, res);
+  });
+};
+
+export default routeController;
