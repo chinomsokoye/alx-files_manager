1b2c73831c4da2f2e4d0636314ad0d98262c807a
nomso <chinomsokoye@yahoo.com>
2023-03-08 04:10:05 -0800

    Create First file

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
 const router = express.Router();
 
@@ -32,9 +33,30 @@ const routeController = (app) => {
   router.get('/users/me', (req, res) => {
     UsersController.getMe(req, res);
   });
-  // router.post('/files', (req, res) => {
-  //   FilesController.postUpload(req, res);
-  // });
+
+  router.post('/files', (req, res) => {
+    FilesController.postUpload(req, res);
+  });
+
+  router.get('/files/:id', (req, res) => {
+    FilesController.getShow(req, res);
+  });
+
+  router.get('/files', (req, res) => {
+    FilesController.getIndex(req, res);
+  });
+
+  router.put('/files/:id/publish', (req, res) => {
+    FilesController.putPublish(req, res);
+  });
+
+  router.put('/files/:id/unpublish', (req, res) => {
+    FilesController.putUnpublish(req, res);
+  });
+
+  router.post('/files/:id/data', (req, res) => {
+    FilesController.getFile(req, res);
+  });
 };
 
 export default routeController;
